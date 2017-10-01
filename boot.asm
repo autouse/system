35fb1514eaa6941bc31119eefab3f599a8e470e9
i <i@is-MacBook-Air.local>
2017-10-01 23:31:43 +0300

    system version 0.02

--- boot.asm
+++ boot.asm
@@ -1,3 +1,26 @@
+org 7C00h
+mov si,msg
+call puts
+jmp v001
+msg:
+db 'System v0.02',0
+puts:
+push si
+mov al,byte [cs:si]
+cmp al,0
+je puts.end
+call putc
+pop si
+inc si
+jmp puts
+puts.end:
+ret
+putc:
+mov ah,0Eh
+mov bx,7
+int 10h
+ret
+v001:
 mov ax,0B800h
 mov es,ax
 l:
