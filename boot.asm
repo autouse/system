0f3f534fbe8d46df1f0ddb730075f59cf7749096
i <i@is-MacBook-Air.local>
2017-10-02 17:41:25 +0300

    version 0.03 usb boot ok

--- boot.asm
+++ boot.asm
@@ -1,9 +1,19 @@
 org 7C00h
+mov ah,1
+mov cx,7
+int 10h
+mov ax, 1003h
+mov bx, 0
+int 10h
+mov ah,0
+mov al,6
+;int 10h
+;
 mov si,msg
 call puts
 jmp v001
 msg:
-db 'System v0.02',0
+db 0Dh,0Ah,'Core i386 version 0.03',0Dh,0Ah,0
 puts:
 push si
 mov al,byte [cs:si]
@@ -28,5 +38,7 @@ inc byte [es:0]
 hlt
 jmp l
 
-times 512 - $ + $$ - 2 db 0
+times 512 - $ + $$ - 2 - 64 db 0
+db 80h,0,0,0,7Fh,0,0,0,0,0,0,0,0,0,0,0
+dd 0,0,0,0,0,0,0,0,0,0,0,0
 db 55h, 0AAh
