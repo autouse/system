c1e83d4fe0b2da90b4f5cd4d954d616da13d33d4
i <i@is-MacBook-Air.local>
2017-10-09 16:56:31 +0300

    version 0.1

--- boot.asm
+++ boot.asm
@@ -1,44 +1,57 @@
-org 7C00h
-mov ah,1
-mov cx,7
-int 10h
-mov ax, 1003h
-mov bx, 0
-int 10h
-mov ah,0
-mov al,6
-;int 10h
-;
-mov si,msg
-call puts
-jmp v001
-msg:
-db 0Dh,0Ah,'Core i386 version 0.03',0Dh,0Ah,0
+; system boot record
+    org 7C00h
+; main function
+    mov si,systemMessage
+    call puts
+    call biosSetFullBlockCursor
+inputLoop:
+    call biosKey
+    cmp al,0Dh
+    jne skipNextLine
+    call putc
+    mov al,0Ah
+skipNextLine:
+    call putc
+    jmp inputLoop
+; end execution
+    jmp halt
+; data
+systemMessage:
+    db 0Dh,0Ah,'System i386 version 0.1',0Dh,0Ah,0
+; bios
+biosSetFullBlockCursor:
+    mov cx,7
+    mov ah,1
+    int 10h
+    ret
+biosKey:
+    mov ah,0
+    int 16h
+    ret
+; halt system
+halt:
+    hlt
+    jmp halt
 puts:
-push si
-mov al,byte [cs:si]
-cmp al,0
-je puts.end
-call putc
-pop si
-inc si
-jmp puts
+    push si
+    mov al,byte [cs:si]
+    cmp al,0
+    je puts.end
+    call putc
+    pop si
+    inc si
+    jmp puts
 puts.end:
-ret
+    ret
 putc:
-mov ah,0Eh
-mov bx,7
-int 10h
-ret
-v001:
-mov ax,0B800h
-mov es,ax
-l:
-inc byte [es:0]
-hlt
-jmp l
-
+    mov ah,0Eh
+    mov bx,7
+    int 10h
+    ret
+; some free space
 times 512 - $ + $$ - 2 - 64 db 0
+; active partition type 7F
 db 80h,0,0,0,7Fh,0,0,0,0,0,0,0,0,0,0,0
 dd 0,0,0,0,0,0,0,0,0,0,0,0
+; boot signature
 db 55h, 0AAh
